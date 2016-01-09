383c8b5defe867bad8e47f65a77604e38388cf4e
AmruthPillai <im.amruth@gmail.com>
2016-01-09 19:59:31 +0530

    recording dialog

--- app/src/main/java/com/srinath/hcfab/under25hack/MainActivity.java
+++ app/src/main/java/com/srinath/hcfab/under25hack/MainActivity.java
@@ -5,11 +5,12 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,11 +23,15 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                CustomDialog cd = new CustomDialog(MainActivity.this);
+                cd.show();
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
