0ab3274a672ef9c6a97e4ce45c2d7b4402f31ff8
AmruthPillai <im.amruth@gmail.com>
2016-01-09 20:57:01 +0530

    recording dialog

--- app/src/main/java/com/srinath/hcfab/under25hack/CustomDialog.java
+++ app/src/main/java/com/srinath/hcfab/under25hack/CustomDialog.java
@@ -3,6 +3,7 @@ package com.srinath.hcfab.under25hack;
 import android.app.Dialog;
 import android.content.Context;
 import android.media.MediaRecorder;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Environment;
 import android.util.Log;
@@ -10,8 +11,18 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -20,10 +31,17 @@ import de.hdodenhof.circleimageview.CircleImageView;
  */
 public class CustomDialog extends Dialog {
 
+    String attachmentName = "mySong";
+    String attachmentFileName = "testSong.mp4";
+    String crlf = "\r\n";
+    String twoHyphens = "--";
+    String boundary = "*****";
+    String filename = "";
     boolean isRecorded = false;
     Button ok, cancel;
     CircleImageView record;
     MediaRecorder recorder;
+    File file;
 
     public CustomDialog(Context context) {
         super(context);
@@ -34,6 +52,12 @@ public class CustomDialog extends Dialog {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.custom_dialog);
         ok = (Button) findViewById(R.id.OK);
+        ok.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new uploadSong().execute();
+            }
+        });
         cancel = (Button) findViewById(R.id.cancel);
         record = (CircleImageView) findViewById(R.id.record);
         record.setOnTouchListener(new View.OnTouchListener() {
@@ -42,7 +66,6 @@ public class CustomDialog extends Dialog {
                 switch (event.getAction()) {
                     case MotionEvent.ACTION_DOWN:
                         if (isRecorded) {
-//                            uploadSong();
                         } else {
                             record.setImageResource(R.drawable.ic_mic_none_white_24dp);
                             startRecording();
@@ -70,7 +93,6 @@ public class CustomDialog extends Dialog {
         recorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4);
         recorder.setOutputFile(getFilename());
         recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
-
         try {
             recorder.prepare();
             recorder.start();
@@ -89,13 +111,89 @@ public class CustomDialog extends Dialog {
 
     private String getFilename() {
         String filepath = Environment.getExternalStorageDirectory().getPath();
-        File file = new File(filepath, "Under25");
+        file = new File(filepath, "Under25");
 
         if (!file.exists()) {
             file.mkdirs();
         }
+        filename = "" + System.currentTimeMillis();
+        return (file.getAbsolutePath() + "/" + filename + ".mp4");
+    }
 
-        return (file.getAbsolutePath() + "/" + System.currentTimeMillis() + ".mp4");
+    public class uploadSong extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            File f = new File(file.getAbsolutePath()+"/"+filename+".mp4");
+            try {
+                HttpURLConnection httpUrlConnection;
+                URL url = new URL("http://204.152.203.111/insert_song.php");
+                httpUrlConnection = (HttpURLConnection) url.openConnection();
+                httpUrlConnection.setUseCaches(false);
+                httpUrlConnection.setDoOutput(true);
+
+                httpUrlConnection.setRequestMethod("POST");
+                httpUrlConnection.setRequestProperty("Connection", "Keep-Alive");
+                httpUrlConnection.setRequestProperty("Cache-Control", "no-cache");
+                httpUrlConnection.setRequestProperty(
+                        "Content-Type", "multipart/form-data;boundary=" + boundary);
+
+                DataOutputStream request = new DataOutputStream(
+                        httpUrlConnection.getOutputStream());
+
+                request.writeBytes(twoHyphens + boundary + crlf);
+                request.writeBytes("Content-Disposition: form-data; name=\"" +
+                        attachmentName + "\";filename=\"" +
+                        attachmentFileName + "\"" + crlf);
+                request.writeBytes(crlf);
+
+
+                byte[] pixels;
+                pixels = new byte[Math.min((int) f.length(), 20 * 1024)];
+
+                int length;
+                Log.d("whatisfile",""+f);
+                FileInputStream fileInputStream = new FileInputStream(f);
+                while ((length = fileInputStream.read(pixels)) != -1) {
+                    request.write(pixels, 0, length);
+                }
+
+                request.write(pixels);
+
+                request.writeBytes(crlf);
+                request.writeBytes(twoHyphens + boundary +
+                        twoHyphens + crlf);
+
+                request.flush();
+                request.close();
+
+                InputStream responseStream = new
+                        BufferedInputStream(httpUrlConnection.getInputStream());
+
+                BufferedReader responseStreamReader =
+                        new BufferedReader(new InputStreamReader(responseStream));
+
+                String line = "";
+                StringBuilder stringBuilder = new StringBuilder();
+
+                while ((line = responseStreamReader.readLine()) != null) {
+                    stringBuilder.append(line).append("\n");
+                }
+                responseStreamReader.close();
+
+                String response = stringBuilder.toString();
+                Log.d("response", response);
+                responseStream.close();
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
     }
 
 }
